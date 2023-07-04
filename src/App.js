b29980155c4d26f7ce9d42dde0531433367ad2c9
rahul4800 <rahul.iraveni4800@gmail.com>
2023-07-04 17:52:27 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import "./App.css";
+import Weathercard from "./components/Weathercard";
+
+const App = () => {
+  const[searchValue, setSearchValue] = useState("pune");
+  const[tempInfo, setTempInfo] = useState("");
+
+  const getWeatherInfo = async () => {
+    try{
+      let API_URL = process.env.REACT_APP_OPENWEATHER_API_KEY;
+      let API_KEY = process.env.REACT_APP_WEATHER_API_ID;
+
+      const url =  `${API_URL}?q=${searchValue}&units=metric&appid=${API_KEY}`;
+      
+      let res = await fetch(url); 
+      let data = await res.json();
+
+      const{ temp, humidity, pressure } = data.main;
+      const{ main:weathermood } = data.weather[0];
+      const{name} = data;
+      const{speed} = data.wind;
+      const{country, sunset} = data.sys;
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+      //console.log(temp);
+      //console.log(data);
+      setTempInfo(myNewWeatherInfo);
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search</button>
+        </div>
+      </div>
+      <Weathercard {...tempInfo} />
+    </>
+
   );
 }
 
